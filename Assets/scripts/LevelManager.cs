f87ae7889f6e4110bce9c1e8966439646dca971e
ItayYagil <132233086+ItayYagil@users.noreply.github.com>
2025-08-03 19:47:47 +0300

    Update LevelManager.cs

--- Assets/scripts/LevelManager.cs
+++ Assets/scripts/LevelManager.cs
@@ -104,12 +104,12 @@ public class LevelManager : MonoBehaviour
 
         "e4,f2,e2,e4,j2,e2," +
         "e4,f2,e2,e4,j2,f2," +
+        "e4,j2,e2,e4,f2,e2," +
+        "e4,j2,e2,e4,f2,j2," +
         "e4,f2,e2,e4,j2,e2," +
         "e4,f2,e2,e4,j2,f2," +
-        "e4,f2,e2,e4,j2,e2," +
-        "e4,f2,e2,e4,j2,f2," +
-        "e4,f2,e2,e4,j2,e2," +
-        "f1,j1,f1,e1,j1,f1,j1,e1,e1,f2,j2,f1,j2," +
+        "e4,j2,e2,e4,f2,e2," +
+        "j1,f1,j1,e1,f1,j1,f1,e1,e1,j2,f2,j1,f2," +
 
         "s2,e2,f2,e2,e4,j2,f2," +
         "e2,e2,j2,e2,e4,f2,j2," +
