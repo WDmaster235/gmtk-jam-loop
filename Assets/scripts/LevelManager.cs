c056246fd7c8f844acd138e803566a67ecc71493
WDmaster235 <shalevikichli@gmail.com>
2026-03-16 22:57:38 +0200

    Revert "Merge branch 'main' of https://github.com/WDmaster235/gmtk-jam-loop"
    This reverts commit 0cfa88c526afccf04756ac17ef1023f7956432cd, reversing
    changes made to bb70d46cf8e3870d971ae46cac070bc603e22d19.

--- Assets/scripts/LevelManager.cs
+++ Assets/scripts/LevelManager.cs
@@ -9,105 +9,96 @@ public class LevelManager : MonoBehaviour
     {
         f,
         j,
-        s, // space
-        e  // empty
+        s, //space
+        e  //empty
     }
 
     [SerializeField] private int HealingAmount;
     [SerializeField] private int DamageAmount;
     [SerializeField] public int Health;
-    [SerializeField] public SpriteRenderer hulaGirlSpriteRenderer;
 
     [Header("Animation")]
     [SerializeField] Animator playerAnimator;
     [SerializeField] PlayerAnimationHandler playerAnimationHandler;
     [SerializeField] Animator hulaHoopAnimator;
 
-    [SerializeField] SpriteRenderer hulaBorderRenderer;
-    [SerializeField] Sprite spriteBorder1;
-    [SerializeField] Sprite spriteBorder2;
-
     [Header("Music")]
     [SerializeField] BeatManager beatManager;
+
 
     [HideInInspector] public int BeatLength { get; set; }
     private int ArrIndex = 0;
     public PosInputs PlayerInput { get; private set; }
     private int currTupBeatLength;
-    private float damageTimer;
-    private bool isDamaged;
     private bool hasCurrKeyBeenPressed;
     [SerializeField] int levelNum;
     string[] levelData;
-    string[] nextLevel = { "Level 2", "Main Menu" };
+    string[] nextLevel = {"Level 2", "Main Menu"};
     string[] currentLevel = { "Level 1", "Level 2" };
-    string currLevelInputs;
+    string currLevelInputs; //string format: "wantedInput""length", ... exmpale: a2,d3,e4,s1
 
     void Start()
     {
-        isDamaged = false;
         levelData = new string[3];
         hasCurrKeyBeenPressed = false;
+        levelData[0] = "e8,e8," +
 
+            "f2,e2,j2,e4,f2,j2,e2," +
+            "f2,e2,j2,e4,f2,j2,e2," +
+            "f2,e2,j2,e4,f2,j2,e2," +
+            "e2,f2,e2,j2,e2,f2,e2,j2," +
+            "f2,e2,j2,e4,f2,j2,e2," +
+            "f2,e2,j2,e4,f2,j2,e2," +
+            "e2,f2,e2,j2,e2,f2,e4," +
+            "e2,j2,e2,f2,e2,j2,e4," +
 
-    levelData[0] = "e8,e8," +
+            "e4,f2,e2,j2,e2,f2,e2," +
+            "e4,j2,e2,f2,e2,j2,e2," +
+            "e4,f2,e2,j2,e2,f2,e2," +
+            "j2,e2,f2,e2,j2,e2,f2,e2," +
+            "e4,j2,e2,f2,e2,j2,e2," +
+            "f2,e2,j2,e4,f2,e4," +
+            "e4,j2,e2,f2,e2,j2,e2," +
+            "e4,f2,e2,j2,e2,f2,e2," +
 
-    "f2,e2,j2,e4,f2,j2,e2," +
-    "f2,e2,j2,e4,f2,j2,e2," +
-    "f2,e2,j2,e4,f2,j2,e2," +
-    "e2,f2,e2,j2,e2,f2,e2,j2," +
-    "f2,e2,j2,e4,f2,j2,e2," +
-    "f2,e2,j2,e4,f2,j2,e2," +
-    "e2,f2,e2,j2,e2,f2,e4," +
-    "e2,j2,e2,f2,e2,j2,e4," +
+            "e4,j2,e2,f2,e2,j2,e2," +
+            "e4,f2,e2,j2,e2,f2,e2," +
+            "e4,j2,e2,e4,f2,e2," +
+            "j2,e2,f2,e2,j2,e2,f2,e2," +
+            "e4,j2,e2,f2,e2,j2,e2," +
+            "f2,e2,j2,e4,f2,e4," +
+            "e4,j2,e2,e4,f2,e2," +
+            "e4,j2,e2,f2,e2,j2,e2," +
 
-    "e4,f2,e2,j2,e2,f2,e2," +
-    "e4,j2,e2,f2,e2,j2,e2," +
-    "e4,f2,e2,j2,e2,f2,e2," +
-    "j2,e2,f2,e2,j2,e2,f2,e2," +
-    "e4,j2,e2,f2,e2,j2,e2," +
-    "f2,e2,j2,e4,f2,e4," +
-    "e4,j2,e2,f2,e2,j2,e2," +
-    "e4,f2,e2,j2,e2,f2,e2," +
+            "f2,e2,j2,e2,f2,e2,j2,e2," +
+            "f2,e4,s2,e8," +
+            "j2,e2,f2,e2,j2,e2,f2,e2," +
+            "j2,e4,s2,e8," +
+            "f2,e2,j2,e2,f2,e6," +
+            "j2,e2,f2,e2,j2,e6," +
+            "f2,e2,j2,e2,f2,e2,j2,e2," +
+            "e4,f2,e2,j2,e2,f2,e2," +
 
-    "e4,j2,e2,f2,e2,j2,e2," +
-    "e4,f2,e2,j2,e2,f2,e2," +
-    "e4,j2,e2,e4,f2,e2," +
-    "j2,e2,f2,e2,j2,e2,f2,e2," +
-    "e4,j2,e2,f2,e2,j2,e2," +
-    "f2,e2,j2,e4,f2,e4," +
-    "e4,j2,e2,e4,f2,e2," +
-    "e4,j2,e2,f2,e2,j2,e2," +
+            "e2,f2,j2,f2,e2,s2,e4," +
 
-    "f2,e2,j2,e2,f2,e2,j2,e2," +
-    "f2,e4,s2,e8," +
-    "j2,e2,f2,e2,j2,e2,f2,e2," +
-    "j2,e4,s2,e8," +
-    "f2,e2,j2,e2,f2,e6," +
-    "j2,e2,f2,e2,j2,e6," +
-    "f2,e2,j2,e2,f2,e2,j2,e2," +
-    "e4,f2,e2,j2,e2,f2,e2," +
+            "j2,e2,f2,e2,j2,e2,f2,e2," +
+            "j2,e2,f2,e2,j2,e2,s2,e2," +
+            "f2,e2,j2,e2,f2,e2,j2,e2," +
+            "f2,j2,f2,j2,e4,s2,e2," +
 
-    "e2,f2,j2,f2,e2,s2,e4," +
+            "f2,e2,j2,e2,f2,e2,j2,e2," +
+            "f2,e2,j2,e2,f2,e2,s2,e2," +
+            "j2,e2,f2,e2,j2,e2,f2,e2," +
+            "j2,f2,j2,f2,e4,s2,e2," +
 
-    "j2,e2,f2,e2,j2,e2,f2,e2," +
-    "j2,e2,f2,e2,j2,e2,s2,e2," +
-    "f2,e2,j2,e2,f2,e2,j2,e2," +
-    "f2,j2,f2,j2,e4,s2,e2," +
-
-    "f2,e2,j2,e2,f2,e2,j2,e2," +
-    "f2,e2,j2,e2,f2,e2,s2,e2," +
-    "j2,e2,f2,e2,j2,e2,f2,e2," +
-    "j2,f2,j2,f2,e4,s2,e2," +
-
-    "f2,e2,j2,e4,f2,j2,e2," +
-    "f2,e2,j2,e4,f2,j2,e2," +
-    "f2,e2,j2,e4,f2,j2,e2," +
-    "e2,f2,e2,j2,e2,f2,e2,j2," +
-    "f2,e2,j2,e4,f2,j2,e2," +
-    "f2,e2,j2,e4,f2,j2,e2," +
-    "e2,f2,j2,f2,e8," +
-    "e2,j2,f2,j2,e8,";
+            "f2,e2,j2,e4,f2,j2,e2," +
+            "f2,e2,j2,e4,f2,j2,e2," +
+            "f2,e2,j2,e4,f2,j2,e2," +
+            "e2,f2,e2,j2,e2,f2,e2,j2," +
+            "f2,e2,j2,e4,f2,j2,e2," +
+            "f2,e2,j2,e4,f2,j2,e2," +
+            "e2,f2,j2,f2,e8," +
+            "e2,j2,f2,j2,e8,";
 
         levelData[1] = "e8,e8," +
 
@@ -232,63 +223,88 @@ public class LevelManager : MonoBehaviour
 
     void Update()
     {
-        if (Health <= 0)
+        if(Health <= 0) 
         {
             lose();
-        }
-
-        if (isDamaged && damageTimer > 0)
-        {
-            damageTimer -= Time.deltaTime;
-
-            if (damageTimer <= 0)
-            {
-                hulaGirlSpriteRenderer.color = Color.white;
-                isDamaged = false;
-            }
         }
 
         if (ArrIndex < currLevelInputs.Length - 1)
         {
             PlayerInput = PlayerInputs();
+
+            //Debug.Log(PlayerInput.ToString()[0]);
             currTupBeatLength = currLevelInputs[ArrIndex + 1] - '0';
+
+            
 
             if (PlayerInput.ToString()[0] == currLevelInputs[ArrIndex] && !hasCurrKeyBeenPressed && currLevelInputs[ArrIndex] != 'e')
             {
                 Health = Math.Min(30, Health + HealingAmount);
                 hasCurrKeyBeenPressed = true;
             }
+
+            if (PlayerInput.ToString()[0] == currLevelInputs[ArrIndex] && !hasCurrKeyBeenPressed && currLevelInputs[ArrIndex] != 'e')
+            {
+                Health = Math.Min(30, Health + HealingAmount);
+                hasCurrKeyBeenPressed = true;
+            }
+
+
             else if (PlayerInput.ToString()[0] != 'e' && PlayerInput.ToString()[0] != currLevelInputs[ArrIndex])
             {
                 Health -= DamageAmount;
-                DamageAnimation();
             }
 
             if (currTupBeatLength - BeatLength <= 0)
             {
                 BeatLength = 0;
 
+
                 int timeTillNextTap = currTupBeatLength;
-                for (int i = ArrIndex + 3; currLevelInputs[i] != 'j' && currLevelInputs[i] != 'f'; i += 3)
+                
+                for (int i = ArrIndex + 3; currLevelInputs[i] != 'j' && currLevelInputs[i] != 'f'; i+=3)
                 {
-                    timeTillNextTap += currLevelInputs[i + 1] - '0';
+                    timeTillNextTap += currLevelInputs[i+1] - '0';
                 }
 
-                hulaHoopAnimator.speed = 1 / (beatManager.IntervalLength * timeTillNextTap);
-
-                if (!hasCurrKeyBeenPressed && currLevelInputs[ArrIndex] != 'e')
-                {
-                    Health -= DamageAmount;
-                    DamageAnimation();
-                }
+                hulaHoopAnimator.speed = 1/(beatManager.IntervalLength * timeTillNextTap);
+               
+               
+               if (hasCurrKeyBeenPressed == false && currLevelInputs[ArrIndex] != 'e')
+               {
+                   Health -= DamageAmount;
+                   Debug.Log(Health);
+               }
 
                 ArrIndex += 3;
 
                 if (currLevelInputs[ArrIndex] != 'e')
                     Debug.Log(currLevelInputs[ArrIndex]);
-
                 hasCurrKeyBeenPressed = false;
             }
+
+
+            else if (PlayerInput.ToString()[0] != 'e' && PlayerInput.ToString()[0] != currLevelInputs[ArrIndex])
+            {
+                Health -= DamageAmount;
+                Debug.Log(Health);
+            }
+
+            if (currTupBeatLength - BeatLength <= 0)
+            {
+                BeatLength = 0;
+
+                if (hasCurrKeyBeenPressed == false && currLevelInputs[ArrIndex] != 'e')
+                {
+                    Health -= DamageAmount;
+                }
+
+                ArrIndex += 3;
+                if (currLevelInputs[ArrIndex] != 'e')
+                    Debug.Log(currLevelInputs[ArrIndex]);
+                hasCurrKeyBeenPressed = false;
+            }
+            
         }
         else
         {
@@ -296,26 +312,13 @@ public class LevelManager : MonoBehaviour
         }
     }
 
-    private void DamageAnimation()
-    {
-        hulaGirlSpriteRenderer.color = Color.red;
-        isDamaged = true;
-        damageTimer = 1f;
-    }
-
-
-    private void FixedUpdate()
-    {
-        damageTimer -= Time.fixedDeltaTime;
-    }
 
     private void Win()
     {
         SceneManager.LoadScene(nextLevel[levelNum], LoadSceneMode.Single);
     }
 
-
-    private void lose()
+    private void lose() 
     {
         SceneManager.LoadScene(currentLevel[levelNum], LoadSceneMode.Single);
     }
@@ -332,9 +335,8 @@ public class LevelManager : MonoBehaviour
             playerAnimator.Play("HipRight");
             return PosInputs.j;
         }
-        else if (Input.GetKeyDown(KeyCode.Space))
+        else if (Input.GetKeyDown("space"))
         {
-            playerAnimator.Play("DodgeAnimation"); 
             return PosInputs.s;
         }
         else
@@ -342,5 +344,4 @@ public class LevelManager : MonoBehaviour
             return PosInputs.e;
         }
     }
-
 }
